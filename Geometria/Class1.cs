53cf34e5feb94a341a65de7f662a3b835223e4c7
elvecho <100934190+elvecho@users.noreply.github.com>
2022-04-07 18:15:12 +0200

    Add project files.

---
+++ Geometria/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lamiaclasse
+{
+    public class Rettangolo
+    {
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+        
+
+
+
+       public Rettangolo(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+
+        }
+        public int CalcolaArea(int baseRettangolo, int altezzaRettangolo)
+        {
+            
+           int Area =  baseRettangolo * altezzaRettangolo;
+           
+            return Area;
+
+        }
+        public int CalcolaPerimetro(int baseRettangolo, int alezzaRettangolo)
+        {
+            int Perimetro = baseRettangolo + alezzaRettangolo;
+            
+            return Perimetro;
+        }
+        public void StampaRettangolo()
+        {
+
+            
+            
+                Console.WriteLine("--Rettangolo--");
+
+                Console.WriteLine("base: " + baseRettangolo + "cm");
+                Console.WriteLine("altezza: " + altezzaRettangolo + "cm");
+                Console.WriteLine("perimetro: " + CalcolaPerimetro);
+                Console.WriteLine("perimetro: " + CalcolaArea);
+            
+            
+        }
+    }
+}
---
+++ Geometria/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using lamiaclasse;
+
+Console.WriteLine("inserisci la base del rettangolo: ");
+int baseR = int.Parse(Console.ReadLine());
+Console.WriteLine("inserisci l altezza del rettangolo: ");
+int altezzaR = int.Parse(Console.ReadLine());
+
+
+Rettangolo NuovoRettangolo = new Rettangolo(baseR, altezzaR);
+
+int area = NuovoRettangolo.CalcolaArea(baseR, altezzaR);
+Console.WriteLine(area);
+int perimetro = NuovoRettangolo.CalcolaPerimetro(baseR, altezzaR);
+Console.WriteLine(perimetro);
+NuovoRettangolo.StampaRettangolo();
+
+
+for(int i = 0; i < 4; i++)
+{
+    Console.WriteLine("inserisci la base del rettangolo: ");
+    int baseR2 = int.Parse(Console.ReadLine());
+    Console.WriteLine("inserisci l altezza del rettangolo: ");
+    int altezzaR2 = int.Parse(Console.ReadLine());
+
+    Rettangolo rettangolo2 = new Rettangolo(baseR2, altezzaR2);
+int area2 = rettangolo2.CalcolaArea(baseR2,altezzaR2);
+    
+    int perimetro2 = rettangolo2.CalcolaPerimetro(baseR2, altezzaR2);
+    rettangolo2.StampaRettangolo();
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
